7c8423ae06562b49925864105f60c4e8b66e3344
ASIQE <87054231+ASIQE@users.noreply.github.com>
2022-02-27 22:42:47 +0800

    Update XMind.js

--- js/backup/XMind.js
+++ js/backup/XMind.js
@@ -2,7 +2,7 @@
 Xmind Unlock annual subscriptions （by LTribe）
 [rewrite_local]
 # Xmind Unlock annual subscriptions （by LTribe）
-https?:\/\/.*\.xmind\..*\/_res\/devices url script-response-body XMind.js
+https?:\/\/.*\.xmind\..*\/_res\/devices url script-response-body https://raw.githubusercontent.com/SIMPO18/Script/8884a193c4beb75efd83662a1c66a5de9d816890/unlock/xmind.js
 [mitm]
 hostname = *.xmind.*,
 */
